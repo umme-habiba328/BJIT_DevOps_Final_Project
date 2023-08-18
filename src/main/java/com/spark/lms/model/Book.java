b246cfc689d72e152f720c224558e7c4f1604fc0
Umme Habiba <131146081+umme-habiba328@users.noreply.github.com>
2023-08-18 10:54:08 +0600

    Update Book.java

--- src/main/java/com/spark/lms/model/Book.java
+++ src/main/java/com/spark/lms/model/Book.java
@@ -3,7 +3,14 @@ package com.spark.lms.model;
 import java.io.Serializable;
 import java.util.Date;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
